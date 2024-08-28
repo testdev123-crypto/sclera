79192d12581d7e9988b01a7fe83f6836dbd61f71
Rohan <rohanpai126@gmail.com>
2024-08-28 16:13:19 +0530

    school chsnges

--- src/main/java/com/example/School/SchoolApplication.java
+++ src/main/java/com/example/School/SchoolApplication.java
@@ -3,10 +3,19 @@ package com.example.School;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class SchoolApplication {
 
 	public static void main(String[] args) {
+		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/school", "root", "mypass123")) {
+			System.out.println("Connection successful!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
